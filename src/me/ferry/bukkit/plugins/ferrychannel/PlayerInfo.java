e163516610bde29da6f9657649d0e508aff626bd
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-03-22 21:46:16 +0100

    Removed unused imports

--- src/me/ferry/bukkit/plugins/ferrychannel/PlayerInfo.java
+++ src/me/ferry/bukkit/plugins/ferrychannel/PlayerInfo.java
@@ -4,15 +4,11 @@
  */
 package me.ferry.bukkit.plugins.ferrychannel;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CopyOnWriteArraySet;
 import org.bukkit.configuration.ConfigurationSection;
 
 /**
